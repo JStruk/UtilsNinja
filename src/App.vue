4820a6fe6bcfc09a8b2cfd25cf80f4dc8ecbc969
JStruk <strukjustin@gmail.com>
2022-10-06 15:50:04 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <!-- <h1> Hello, World! </h1> -->
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <!-- <RouterLink to="/about">About</RouterLink> -->
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
