d0d7195c2006a14c3fcf3c617afccd7aa2ff2879
JStruk <strukjustin@gmail.com>
2022-10-17 17:00:24 -0400

    Refactor to composition API
    * also fixed ts error on build

--- src/views/LoremIpsumGenerator.vue
+++ src/views/LoremIpsumGenerator.vue
@@ -2,50 +2,43 @@
   <div class="">
     <div class="bg-white shadow rounded p-5">
       <div class="flex justify-evenly w-4/5">
-        <input v-model="number" type="number" placeholder="Paragraphs" class="w-full" />
-        <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button" @click="generateLorem(number)">Generate</button>
-        <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button" @click="copyToClipboard(lorem)">Copy to Clipboard</button>
+        <input v-model="numOfParagraphs" type="number" placeholder="Paragraphs" class="w-full" />
+        <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button"
+          @click="generateLorem">Generate</button>
+        <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button"
+          @click="copyToClipboard(lorem)">Copy to Clipboard</button>
       </div>
-      <textarea
-        :value="lorem"
-        disabled
-        rows="30"
+      <textarea :value="lorem" disabled rows="30"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-        placeholder="Lorem Ipsum.."
-      ></textarea>
+        placeholder="Lorem Ipsum.."></textarea>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { LoremIpsum } from "lorem-ipsum";
+import { ref } from 'vue';
 
-export default {
-  data() {
-    return {
-      lorem: "",
-      number: "",
-    };
-  },
-  created() {},
-  methods: {
-    generateLorem(str: string) {
-      const lorem = new LoremIpsum();
-      var num: number = +str;
-      this.lorem = lorem.generateParagraphs(num);
+const lorem = ref<string>('')
+const numOfParagraphs = ref<number>(0)
+
+function generateLorem() {
+  const loremIpsumGenerator = new LoremIpsum();
+  lorem.value = loremIpsumGenerator.generateParagraphs(numOfParagraphs.value);
+}
+
+function copyToClipboard(str: string) {
+  navigator.clipboard.writeText(str).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
     },
-    copyToClipboard(str: string) {
-      navigator.clipboard.writeText(str).then(
-        function () {
-          console.log("Async: Copying to clipboard was successful!");
-        },
-        function (err) {
-          console.error("Async: Could not copy text: ", err);
-        }
-      );
-    },
-  },
-};
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
