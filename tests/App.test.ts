44e30db054f2941994ffa41e1fe0cc27eaf379a9
JStruk <strukjustin@gmail.com>
2022-10-20 12:28:47 -0400

    add App tests

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,7 +1,28 @@
+// @vitest-environment jsdom
+
 import { describe, expect, it } from 'vitest';
+import App from '@/App.vue'
+import { mount } from "@vue/test-utils";
+import { router } from "./helpers/testRouter";
+
+const renderComponent = () => {
+    return mount(App, {
+        global: {
+            plugins: [router]
+        }
+    })
+}
 
 describe('App', () => {
-    it('does nothing yet', () => {
-        expect(true).toBeTruthy();
+    it('renders the component', () => {
+        const wrapper = renderComponent();
+
+        expect(wrapper.exists()).toBeTruthy();
+    })
+
+    it('renders a navbar', () => {
+        const wrapper = renderComponent();
+
+        expect(wrapper.find('nav').exists()).toBeTruthy();
     })
 })
