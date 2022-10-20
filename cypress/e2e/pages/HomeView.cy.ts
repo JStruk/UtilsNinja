7948b58ad3c8408e6e8aa63c8920f718fdd1ec40
JStruk <strukjustin@gmail.com>
2022-10-20 14:09:30 -0400

    cypress tests for HomeView page

---
+++ cypress/e2e/pages/HomeView.cy.ts
@@ -0,0 +1,32 @@
+import { slogan } from "../../../src/constants/constants";
+
+describe('HomeView', () => {
+    it('shows the title', () => {
+        cy.visit('/')
+        cy.contains('.md\\:text-8xl', 'UtilsNinja')
+    })
+
+    it('shows the slogan', () => {
+        cy.visit('/')
+        cy.contains(slogan)
+    })
+
+    it('shows the CTA button', () => {
+        cy.visit('/')
+        cy.contains('View Tools')
+    })
+
+    it('allows the user to go to the Tools page via CTA button', () => {
+        cy.visit('/')
+        cy.contains('View Tools').click()
+
+        cy.url().should('be.equal', `${Cypress.config("baseUrl")}/tools`)
+    })
+
+    it('allows the user to go to the Tools page via navbar', () => {
+        cy.visit('/')
+        cy.contains('Tools').click()
+
+        cy.url().should('be.equal', `${Cypress.config("baseUrl")}/tools`)
+    })
+})
