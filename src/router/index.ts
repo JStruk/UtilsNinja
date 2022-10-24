875889bec5810d2d7d4363738cef9015930b3c8d
JStruk <strukjustin@gmail.com>
2022-10-24 16:38:24 -0400

    cleanup + strictly type routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,44 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import ToolsView from "../views/ToolsView.vue";
-import LoremIpsumGenerator from "../views/LoremIpsumGenerator.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+import ToolsView from '../views/ToolsView.vue';
+import LoremIpsumGenerator from '../views/LoremIpsumGenerator.vue';
 
-export const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/tools",
-    name: "tools",
-    component: ToolsView,
-    children: [
-      {
-        path: "lig",
-        name: "LoremIpsumGenerator",
-        component: LoremIpsumGenerator,
-      },
-    ],
-  },
+export const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+    },
+    {
+        path: '/tools',
+        name: 'tools',
+        component: ToolsView,
+        children: [
+            {
+                path: 'lig',
+                name: 'LoremIpsumGenerator',
+                component: LoremIpsumGenerator,
+                meta: {
+                    tabDisplayName: 'Lorem Ipsum'
+                }
+            },
+        ],
+    },
 
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (About.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import('../views/AboutView.vue')
-  // }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 });
 
 export default router;
