4167537f85e7ca868382776bc39c121cc113ea8b
JStruk <strukjustin@gmail.com>
2022-10-31 16:48:39 -0400

    extract csv-to-json utility logic and test
    * happy path test for now

---
+++ src/utilities/csvtojson.ts
@@ -0,0 +1,11 @@
+export function convertCSVToJSON(csv: string): string {
+    const all_data = csv.split('\n')
+    const headers = all_data[0].split(',')
+
+    const json = all_data.slice(1).map(line => {
+        const fields = line.split(',')
+        return Object.fromEntries(headers.map((h, i) => [h, fields[i]]))
+    })
+
+    return JSON.stringify(json, null, '\t')
+}
