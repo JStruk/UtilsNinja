3d1706ed8cffa61ec5f3c06cf8bbe38e5ba48292
Justin Struk <strukjustin@gmail.com>
2022-12-20 16:10:50 -0500

    extract csv-to-json utility logic and test (#23)
    * happy path test for now

--- src/views/CsvToJson.vue
+++ src/views/CsvToJson.vue
@@ -1,39 +1,28 @@
 <template>
-    <div class="">
-        <div class="bg-white shadow rounded p-5">
-            <div class="flex justify-evenly w-4/5">
-                <textarea v-model="csv_data" type="text" placeholder="CSV Data" class="w-full"></textarea>
-                <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button"
-                @click="convert">Convert</button>
-            </div>
-            <textarea :value="json_value_str" disabled rows="30" 
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" 
-            placeholder="JSON format"></textarea>
-        </div>
+  <div class="">
+    <div class="bg-white shadow rounded p-5">
+      <div class="flex justify-evenly w-4/5">
+        <textarea v-model="CSVData" type="text" placeholder="CSV Data" rows="5" class="w-full p-2"></textarea>
+        <button class="bg-indigo-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg w-28" type="button"
+                @click="convert">Convert
+        </button>
+      </div>
+      <textarea :value="jsonString" readonly rows="30"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="JSON format"></textarea>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 
 import { ref } from 'vue';
+import { convertCSVToJSON } from "@/utilities/csvtojson";
 
-const json_value = ref<Array<Object>>([])
-const csv_data = ref<string>('')
-const json_value_str = ref<string>('')
+const CSVData = ref<string>('')
+const jsonString = ref<string>('')
 
 function convert() {
-    const all_data = csv_data.value.split('\n')
-    const headers = all_data[0].split(',')
-    json_value.value = all_data.slice(1).map(line => {
-        const fields = line.split(',')
-        return Object.fromEntries(headers.map((h, i) => [h, fields[i]]))
-    })
-    json_value_str.value = JSON.stringify(json_value.value, null, "\t")
+  jsonString.value = convertCSVToJSON(CSVData.value)
 }
-
-console.log(json_value)
 </script>
-
-<style>
-
-</style>
---
+++ tests/utilities/csvtojson.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest'
+import { convertCSVToJSON } from "@/utilities/csvtojson";
+
+describe('CSVToJSON', () => {
+    it('should convert valid CSV to valid JSON string', () => {
+        const CSVData = "Name,Age,Height\nJoe,25,5'10\nMike,30,6'0"
+
+        const expectedJSON = [
+            {
+                Name: 'Joe',
+                Age: '25',
+                Height: "5'10"
+            },
+            {
+                Name: 'Mike',
+                Age: '30',
+                Height: "6'0"
+            }
+        ]
+
+        expect(convertCSVToJSON(CSVData)).toEqual(JSON.stringify(expectedJSON, null, '\t'))
+    })
+})
