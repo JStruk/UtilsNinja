69edde440d2533ea3dbd68adbc71c7cddbad3793
JStruk <strukjustin@gmail.com>
2023-01-09 16:20:31 -0500

    refactor formatJSON util

--- src/views/FormatJSON.vue
+++ src/views/FormatJSON.vue
@@ -2,18 +2,14 @@
   <div class="flex items-center justify-center px-4 space-x-4 h-full overflow-hidden">
     <div class="flex-1 rounded-lg p-2 min-h-1/2 h-full">
       <span>Enter JSON Data:</span>
-      <textarea rows=40 v-model="inputText" type="text" placeholder="JSON Data"
-                class="w-full border border-gray-400 rounded-lg p-2"/>
+      <textarea rows=40 v-model="inputText" @input="formatJSON" type="text" placeholder="JSON Data"
+        class="w-full border border-gray-400 rounded-lg p-2" />
     </div>
 
     <div class="flex-1 p-2 h-full overflow-scroll">
-      <span>Formatted JSON: (valid: {{isJSONValid}}})</span>
+      <span>Formatted JSON: (valid: {{ isJSONValid }}})</span>
       <div class="border border-gray-400 rounded-lg p-4">
-        <vue-json-pretty
-            showLineNumber
-            showLine
-            showIcon
-            :data="formattedJSON"/>
+        <vue-json-pretty showLineNumber showLine showIcon :data="formattedJSON" />
       </div>
     </div>
   </div>
@@ -22,31 +18,24 @@
 </template>
 
 <script lang="ts" setup>
-import {computed, ref} from 'vue';
+import { computed, ref } from 'vue';
 import VueJsonPretty from 'vue-json-pretty';
 import 'vue-json-pretty/lib/styles.css';
 
 const inputText = ref<string>('{}')
 const isJSONValid = ref<boolean>(false);
+const formattedJSON = ref<string>('');
 
-console.log(JSON.stringify({ 'this': 'is stringified' }));
-
-const formattedJSON = computed(() => {
-
+function formatJSON() {
+  inputText.value.replace(/'/g, '"')
   try {
-    console.log(inputText.value);
-    inputText.value.replace(/'/g, '"')
-    const parsed = JSON.parse(inputText.value);
-    // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+    formattedJSON.value = JSON.parse(inputText.value);
     isJSONValid.value = true;
-    return parsed;
   } catch (e) {
-    console.log(e);
-    // eslint-disable-next-line vue/no-side-effects-in-computed-properties
     isJSONValid.value = false;
-    return {}
   }
-})
+}
+
 </script>
 
 <style>
