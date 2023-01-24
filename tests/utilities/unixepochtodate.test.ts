c1be0ab94251428e83bb1457621ca672ddfb2c9a
JStruk <strukjustin@gmail.com>
2023-01-24 17:01:59 -0500

    added unix epoch to date string converter utility

---
+++ tests/utilities/unixepochtodate.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import { unixEpochToDate } from '@/utilities/unixEpochToDate.ts';
+
+describe('Unix Epoch to Date', () => {
+    it('foobar', () => {
+
+    })
+})
