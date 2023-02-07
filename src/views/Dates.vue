82aadcb210fb372653916aa433ae7f31e90de1e9
JStruk <strukjustin@gmail.com>
2023-02-07 17:04:55 -0500

    remove extra }

--- src/views/Dates.vue
+++ src/views/Dates.vue
@@ -3,13 +3,13 @@
     <div class="flex-1 rounded-lg p-2 min-h-1/2 h-full">
       <span>Enter timestamp:</span>
       <input v-model="inputText" @input="formatDate" type="text" placeholder="Timestamp"
-                class="w-full border border-gray-400 rounded-lg p-2" />
+             class="w-full border border-gray-400 rounded-lg p-2"/>
     </div>
 
     <div class="flex-1 p-2 h-full overflow-scroll">
-      <span>Formatted Dates: (valid: {{ isTimestampValid }}})</span>
+      <span>Formatted Dates: (valid: {{ isTimestampValid }})</span>
       <div class="border border-gray-400 rounded-lg p-4">
-        <vue-json-pretty showLineNumber showLine showIcon :data="formattedDate" />
+        <vue-json-pretty showLineNumber showLine showIcon :data="formattedDate"/>
       </div>
     </div>
   </div>
