ff4593e223be55ef0a7293085abdb1861d8baf26
JStruk <strukjustin@gmail.com>
2023-02-15 16:35:54 -0500

    add cypress tests for StringInspector

--- cypress/e2e/utils/StringInspector.cy.ts
+++ cypress/e2e/utils/StringInspector.cy.ts
@@ -1,11 +1,39 @@
+import { inspect, InspectionResult } from '../../../src/utilities/StringInspector';
+
 describe('String Inspector', () => {
     beforeEach(() => {
         cy.visit('/tools/inspector')
     })
 
     const text = 'idk this is definitely some text';
+    const sentence = 'this has \n four of the same word \r and that word is word word';
+
 
     it('should allow the user to enter text', () => {
         cy.get('textarea').type(text)
     })
+
+    it('should display the results of the inspected string', () => {
+        cy.get('textarea').type(sentence)
+
+        const inspectorResult: InspectionResult = inspect(sentence)
+
+        cy.contains(`"characterCount":${inspectorResult.characterCount}`)
+        cy.contains(`"wordCount":${inspectorResult.wordCount}`)
+        cy.contains(`"nonWhitespaceCharacterCount":${inspectorResult.nonWhitespaceCharacterCount}`)
+        cy.contains(`"lineCount":${inspectorResult.lineCount}`)
+    })
+
+    it('should display the word distribution for the inspected string', function () {
+        cy.get('textarea').type(sentence)
+
+        const inspectorResult: InspectionResult = inspect(sentence)
+
+        cy.contains('"wordDistribution":{')
+        const distribution = inspectorResult.wordDistribution
+
+        Object.keys(distribution).map((word) => {
+            cy.contains(`"${word}":${distribution[word]}`)
+        })
+    });
 })
