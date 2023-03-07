e11c10560773551250073f34f2e4bf166d4ef7c9
JStruk <strukjustin@gmail.com>
2023-03-07 16:30:38 -0500

    Cleans up ToolsView

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,90 +1,12 @@
 <template>
   <ul class="flex border-b pt-6">
-    <li :class="{ '-mb-px mr-1': activeTab === 0 }" class="mr-1">
+    <li v-for="(link, index) in links" :key="index" :class="{ '-mb-px mr-1': activeTab === index }" class="mr-1">
       <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 0 }"
+          :to="{ name: link.route }"
+          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === index }"
           class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(0)"
-          :to="{ name: 'FormatJson' }"
-      >JSON Format
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 1 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 1 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(1)"
-          :to="{ name: 'tools' }"
-      >Base 64 Encode/Decode
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 2 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 2 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(2)"
-          :to="{ name: 'LoremIpsumGenerator' }"
-      >Lorem Ipsum
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 3 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 3 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(3)"
-          :to="{ name: 'JsonToCSV' }"
-      >JSON to CSV
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 4 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 4 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(4)"
-          :to="{ name: 'CsvToJson' }"
-      >CSV to JSON
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 5 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 5 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(5)"
-          :to="{ name: 'Inspector' }"
-      >Inspector
-      </router-link
-      >
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 6 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 6 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(6)"
-          :to="{ name: 'Dates' }">
-        Dates
-      </router-link>
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 7 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 7 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(7)"
-          :to="{ name: 'PHPToJson' }">
-        PHP To JSON
-      </router-link>
-    </li>
-    <li :class="{ '-mb-px mr-1': activeTab === 8 }" class="mr-1">
-      <router-link
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === 8 }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(8)"
-          :to="{ name: 'JSONToPHP' }">
-         JSON To PHP
+          @click="() => setActiveTab(index)"
+      >{{ link.label }}
       </router-link>
     </li>
   </ul>
@@ -99,4 +21,16 @@ let activeTab = ref(0);
 const setActiveTab = (tabNumber: number) => {
   activeTab.value = tabNumber;
 };
+
+const links = [
+  { route: 'FormatJson', label: 'JSON Format' },
+  { route: 'tools', label: 'Base 64 Encode/Decode' },
+  { route: 'LoremIpsumGenerator', label: 'Lorem Ipsum' },
+  { route: 'JsonToCSV', label: 'JSON to CSV' },
+  { route: 'CsvToJson', label: 'CSV to JSON' },
+  { route: 'Inspector', label: 'Inspector' },
+  { route: 'Dates', label: 'Dates' },
+  { route: 'PHPToJson', label: 'PHP To JSON' },
+  { route: 'JSONToPHP', label: 'JSON To PHP' },
+];
 </script>
