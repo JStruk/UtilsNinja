20ddcc3d3e6dfe5570d0ef98f585462ee1adcfa8
JStruk <strukjustin@gmail.com>
2023-09-26 15:58:56 -0400

    Add Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style.css'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(VueGtag,
+    {
+        appName: 'UtilsNinja',
+        pageTrackerScreenviewEnabled: true,
+        config: { id: import.meta.env.VITE_GOOGLE_ANALYTICS_MEASUREMENT_ID }
+    },
+    router
+)
 app.mount('#app')
