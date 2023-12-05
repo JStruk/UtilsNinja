872abbe179102f26410a6921283d177d55ba53d6
JStruk <strukjustin@gmail.com>
2023-12-05 16:31:05 -0500

    Add debounce and clipboard buttons

--- src/views/ColorConverter.vue
+++ src/views/ColorConverter.vue
@@ -1,57 +1,77 @@
 <template>
-    <div class="border-2 space-y-4 flex flex-col text-right items-center">
-        <div class="flex">
-            <div class="p-2 w-48">
-                <p>Enter color code:</p>
-                <p class="text-xs">Accept RGB, HEX, CMYK, HSL</p>
-            </div>
-            <input type="text" v-model="colorInput" @input="convert" class="border rounded border-black p-1 w-96">
-        </div>
-        <div class="flex">
-            <p class="text-lg w-48 p-2">Color:</p>
-            <input type="text" class="border rounded border-gray-300 p-1 w-96" :style="{ backgroundColor: hex }" readonly>
-        </div>
-        <div class="flex">
-            <p class="text-lg w-48 p-2">RGB:</p>
-            <input type="text" id="rgb" v-model="rgb" class="border rounded border-gray-300 p-1 w-96" readonly>
-        </div>
-        <div class="flex">
-            <p class="text-lg w-48 p-2">HEX:</p>
-            <input type="text" id="hex" v-model="hex" class="border rounded border-gray-300 p-1 w-96" readonly>
-        </div>
-        <div class="flex">
-            <p class="text-lg w-48 p-2">CMYK:</p>
-            <input type="text" id="cmyk" v-model="cmyk" class="border rounded border-gray-300 p-1 w-96" readonly>
-        </div>
-        <div class="flex">
-            <p class="text-lg w-48 p-2">HSL:</p>
-            <input type="text" id="hsl" v-model="hsl" class="border rounded border-gray-300 p-1 w-96" readonly>
-        </div>
+  <div class="border-2 space-y-4 flex flex-col text-right items-center text-purple-500">
+    <div class="flex mb-12">
+      <div class="p-2 w-48">
+        <p class="text-xl font-bold">Enter color code:</p>
+        <p class="text-xs">Accept RGB, HEX, CMYK, HSL</p>
+      </div>
+      <div class="flex flex-col overflow-ellipsis">
+        <input type="text" v-model="colorInput" v-debounce:300ms="convert"
+               class="border rounded border-black p-1 w-96 text-black text-xl">
+        <p v-if="errorMessage" class="text-red-500 text-sm text-left">{{ errorMessage }}</p>
+      </div>
     </div>
+    <div class="flex">
+      <p class="text-lg w-48 p-2">Color:</p>
+      <input type="text" class="border rounded border-gray-300 p-1 w-96" :style="{ backgroundColor: hex }" readonly>
+    </div>
+    <div class="flex">
+      <p class="text-lg w-48 p-2">RGB:</p>
+      <input type="text" id="rgb" v-model="rgb" class="border rounded border-gray-300 p-1 w-96 text-black" readonly>
+      <button class="bg-purple-500 hover:bg-purple-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg p-2"
+              type="button" @click="copyToClipboard(rgb)">Copy to Clipboard
+      </button>
+    </div>
+    <div class="flex">
+      <p class="text-lg w-48 p-2">HEX:</p>
+      <input type="text" id="hex" v-model="hex" class="border rounded border-gray-300 p-1 w-96 text-black" readonly>
+      <button class="bg-purple-500 hover:bg-purple-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg p-2"
+              type="button" @click="copyToClipboard(hex)">Copy to Clipboard
+      </button>
+    </div>
+    <div class="flex">
+      <p class="text-lg w-48 p-2">CMYK:</p>
+      <input type="text" id="cmyk" v-model="cmyk" class="border rounded border-gray-300 p-1 w-96 text-black" readonly>
+      <button class="bg-purple-500 hover:bg-purple-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg p-2"
+              type="button" @click="copyToClipboard(cmyk)">Copy to Clipboard
+      </button>
+    </div>
+    <div class="flex">
+      <p class="text-lg w-48 p-2">HSL:</p>
+      <input type="text" id="hsl" v-model="hsl" class="border rounded border-gray-300 p-1 w-96 text-black" readonly>
+      <button class="bg-purple-500 hover:bg-purple-600 text-white text-sm leading-6 font-medium mx-4 rounded-lg p-2"
+              type="button" @click="copyToClipboard(hsl)">Copy to Clipboard
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { ColorConverter } from '@/utilities/ColorConverter'
+import { copyToClipboard } from '@/helpers/CopyToClipboard'
 
 const colorInput = ref<string>('')
 const rgb = ref<string>('')
 const hex = ref<string>('')
 const hsl = ref<string>('')
 const cmyk = ref<string>('')
+let errorMessage = ref<string | null>('')
 
 function convert() {
-    try {
-        const convertedColorCodes = ColorConverter(colorInput.value)
-        rgb.value = convertedColorCodes.rgb
-        hex.value = convertedColorCodes.hex
-        hsl.value = convertedColorCodes.hsl
-        cmyk.value = convertedColorCodes.cmyk
+  try {
+    const convertedColorCodes = ColorConverter(colorInput.value.toLowerCase())
+    errorMessage.value = null
+    rgb.value = convertedColorCodes.rgb
+    hex.value = convertedColorCodes.hex
+    hsl.value = convertedColorCodes.hsl
+    cmyk.value = convertedColorCodes.cmyk
 
-    } catch (e: any) {
-        console.log(e.message)
-        // alert(e.message)
+  } catch (e: any) {
+    if (colorInput.value) {
+      errorMessage.value = e.message
     }
+  }
 }
 
 </script>
