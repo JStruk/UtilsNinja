27db420ac5a61032f5d90e6a4dda412f67bf1795
JStruk <strukjustin@gmail.com>
2024-01-16 16:33:54 -0500

    wip

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,20 +1,35 @@
 <template>
-  <ul class="flex border-b pt-6">
-    <li v-for="(link, index) in links" :key="index" :class="{ '-mb-px mr-1': activeTab === index }" class="mr-1">
-      <router-link
-          :to="{ name: link.route }"
-          :class="{ 'bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-purple-700': activeTab === index }"
-          class="bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800"
-          @click="() => setActiveTab(index)"
-      >{{ link.label }}
-      </router-link>
-    </li>
-  </ul>
-  <router-view class="p-4"></router-view>
+  <div class="flex">
+    <div>
+      <div class="font-extrabold text-4xl text-purple-600 transition-all transform"  @click="() => expanded = !expanded">
+        <p class="w-5" :class="{'rotate-180 duration-200' : expanded }" >&#10095;</p>
+      </div>
+      <div class="pt-4 text-center" :class="{'block' : expanded }">
+        <h2 class="text-xl underline font-bold text-purple-600"> Tools </h2>
+        <ul class="flex border-b pt-2 flex-col">
+          <li v-for="(link, index) in links" :key="index" :class="{ '-mb-px mr-1': activeTab === index }" class="mr-1">
+            <router-link
+                :to="{ name: link.route }"
+                :class=" activeTab === index ? 'bg-purple-600 text-white inline-block border-l border-t border-r rounded-t py-2 px-4'
+              :
+              'bg-white inline-block py-2 px-4 text-purple-500 hover:text-purple-800'"
+                @click="() => setActiveTab(index)"
+            >{{ link.label }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+
+    <router-view class="p-4 flex-grow"></router-view>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+
+let expanded = ref<boolean>(true)
 
 let activeTab = ref(0)
 
