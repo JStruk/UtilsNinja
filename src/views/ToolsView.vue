8b2c7e1859f94d2ca3de54c5c1030b9273f056f7
Justin Struk <10167488+JStruk@users.noreply.github.com>
2024-01-17 15:07:48 -0500

    Implement ToolsView mobile layout
    * no sidebar on mobile, instead show a list of utility cards that are clickable to go to the utility page. If on mobile and not on the '/tools' page, only show the child component and not the parent

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative flex min-h-screen justify-center overflow-hidden bg-[#EDEDED]">
+  <div class="hidden md:flex relative min-h-screen justify-center overflow-hidden bg-[#EDEDED]">
     <div
         class="flex flex-col pt-12 bg-slate-300 bg-opacity-50 text-center overflow-hidden transition-all duration-500 ease-in-out"
         :class="expanded ? 'w-80 px-6' : 'w-0'"
@@ -9,33 +9,56 @@
         <li v-for="(link, index) in links"
             :key="index"
             class="p-1 px-4 font-bold"
-            :class="activeTab === index ? 'bg-purple-600 text-white' : index % 2 === 0 ? 'bg-slate-200 text-purple-600' : 'bg-slate-100 text-purple-600'"
+            :class="activeTab === index ? 'bg-purple-600 text-white' : index % 2 === 0 ? 'bg-slate-100 text-purple-600' : 'bg-slate-200 text-purple-600'"
         >
           <router-link :to="{ name: link.route }" @click="() => setActiveTab(index)">
-            <p v-html="link.label" />
+            <p v-html="link.label"/>
           </router-link>
         </li>
       </ul>
     </div>
     <div
         class="bg-slate-300 bg-opacity-50 p-2 text-purple-700"
+        :title="expanded ? 'collapse' : 'expand'"
         @click="() => expanded = !expanded"
     >
-      <p class="text-4xl" :class="expanded ? 'rotate-180' : ''" >&#10095;</p>
+      <p class="text-4xl" :class="expanded ? 'rotate-180' : ''">&#10095;</p>
     </div>
-
     <div class="flex-1 bg-slate-100">
-      <router-view />
+      <router-view/>
     </div>
   </div>
+
+  <div v-show="isMainToolsPage">
+    <div class="flex flex-col items-center md:hidden text-slate-100 space-y-4 py-12">
+      <div
+          v-for="(link, index) in links"
+          :key="index"
+          class="h-28 w-3/4 bg-purple-700 flex items-center justify-center text-center"
+      >
+        <router-link :to="{ name: link.route }" @click="() => setActiveTab(index)">
+          <p v-html="link.label"/>
+        </router-link>
+      </div>
+    </div>
+  </div>
+  <router-view/>
 </template>
 
 <script lang="ts" setup>
-
-import { ref } from 'vue'
+import { onMounted, onUpdated, ref } from 'vue'
+import { useRoute } from 'vue-router'
 
 let activeTab = ref(0)
 let expanded = ref<boolean>(true)
+let isMainToolsPage = ref<boolean>(true)
+const route = useRoute()
+
+const updateIsMainToolsPage = () => {
+  isMainToolsPage.value = route.path === '/tools'
+}
+onMounted(updateIsMainToolsPage)
+onUpdated(updateIsMainToolsPage)
 
 const setActiveTab = (tabNumber: number) => {
   activeTab.value = tabNumber
@@ -48,7 +71,7 @@ const links = [
   { route: 'CSVToJSON', label: 'CSV &#10140; JSON' },
   { route: 'PHPToJSON', label: 'PHP &#10140; JSON' },
   { route: 'JSONToPHP', label: 'JSON &#10140; PHP' },
-  { route: 'Inspector', label: 'Inspector' },
+  { route: 'Inspector', label: 'String Inspector' },
   { route: 'Dates', label: 'Dates' },
   { route: 'Base64Encode', label: 'Base64 Encode' },
   { route: 'Base64Decode', label: 'Base64 Decode' },
@@ -56,19 +79,3 @@ const links = [
   { route: 'ColorConverter', label: 'Color Converter' }
 ]
 </script>
-
-<style>
-html {
-  background: #efefef;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 1s ease-out;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
