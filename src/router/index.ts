d3710a0407e81cf7318176df3b669245e8995f61
Justin Struk <10167488+JStruk@users.noreply.github.com>
2024-01-24 17:50:28 -0500

    scroll to top of page on navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -96,6 +96,7 @@ export const routes = [
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+    scrollBehavior: () => ({ top: 0 })
 })
 
 export default router
