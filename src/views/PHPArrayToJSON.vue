ab1677aacdd48563d1ae64f4b5bbad2ada43a329
Justin Struk <10167488+JStruk@users.noreply.github.com>
2025-06-05 15:58:29 -0400

    Add user feedback if the PHP array to JSON conversion fails

--- src/views/PHPArrayToJSON.vue
+++ src/views/PHPArrayToJSON.vue
@@ -11,23 +11,29 @@
               class="h-full text-purple-500"
               v-debounce:300ms="convert"
           />
+          <div v-if="error" class="absolute bottom-0 left-0 right-0 bg-red-100 text-red-700 p-2 text-sm">
+            {{ error }}
+          </div>
         </div>
       </div>
     </template>
     <template v-slot:right-pane>
       <vue-json-pretty
+          v-if="!error"
           showLineNumber
           showLine
           showIcon
           :data="json"
           class="flex-1"
       />
+      <div v-else class="flex-1 flex items-center justify-center text-gray-500">
+        Invalid PHP array format
+      </div>
     </template>
   </TwoPaneLayout>
 </template>
 
 <script lang="ts" setup>
-
 import { ref } from 'vue'
 import { PHPArrayToJSON } from '@/utilities/PHPArrayToJSON'
 import VueJsonPretty from 'vue-json-pretty'
@@ -37,12 +43,15 @@ import { VAceEditor } from 'vue3-ace-editor'
 
 const PHPInput = ref<string>('')
 const json = ref()
+const error = ref<string>('')
 
 function convert() {
   try {
+    error.value = ''
     json.value = PHPArrayToJSON(PHPInput.value)
   } catch (e) {
-    // this is just so we don't flood the console
+    error.value = e instanceof Error ? e.message : 'Failed to convert PHP array to JSON'
+    json.value = null
   }
 }
 </script>
